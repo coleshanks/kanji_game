c7ff224ac9a787bcd96b6908739553f34334b8db
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2024-05-24 16:01:13 +0900

    Update kanji_game.py

--- kanji_game.py
+++ kanji_game.py
@@ -35,29 +35,40 @@ def get_random_word(words_list, index):
 def get_random_reading(readings_list, index):
     return readings_list[index]  # Return the reading at the given index
 
-def initialize_game():
-
+def loading_intro_screen():
     time.sleep(0.2)
     print("The game is being initialized")
 
     loading()
 
-    words = read_words(file_path)  # Call read_words and store the result in words
     time.sleep(2)
     print("Building words list...")
+
+    time.sleep(1)
+    print("Building arrays...")
+
+    time.sleep(0.8)
+    print("Reading words/readings into tuples...")
+
+    time.sleep(3)
+    print("Initialization complete...")
+
+    time.sleep(0.3)
+    print("Entering game state...\n")
+    
+
+def initialize_game():
+
+    words = read_words(file_path)  # Call read_words and store the result in words
 
     # Initialize empty lists for words and readings
     words_list = []
     readings_list = []
-    time.sleep(1)
-    print("Building arrays...")
 
     # Simple for loop to iterate over the list of tuples
     for word, reading in words:
         words_list.append(word)  # Append the word to words_list
         readings_list.append(reading)  # Append the reading to readings_list
-    time.sleep(0.8)
-    print("Reading words/readings into tuples...")
 
     # Generate a random index
     word_size = len(words_list)
@@ -67,10 +78,8 @@ def initialize_game():
     random_word = get_random_word(words_list, random_index)
     random_reading = get_random_reading(readings_list, random_index)
 
-    time.sleep(3)
-    print("Initialization complete...")
-    time.sleep(0.3)
-    print("Entering game state...\n")
+    loading_intro_screen()
+
 
     return word_size, words_list, readings_list  # Return as a tuple
 
