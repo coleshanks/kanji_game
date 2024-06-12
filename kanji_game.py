5408cf5dd1e0a767d6cef226e820709a0ccb99cd
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2024-06-12 15:59:20 +0900

    Update kanji_game.py

--- kanji_game.py
+++ kanji_game.py
@@ -212,7 +212,8 @@ def game(word_size, words_list, readings_list, definitions_list):
         answer = input("Enter the reading\n")
         if answer == reading:
             score = score + 1
-            clear()
+            print(answer)
+            clear() #temp
             print(f"{BRIGHT_GREEN}correct!{RESET}")
             print(f"{BRIGHT_CYAN}{word}{RESET} ({definition})\n")
             print(f"Score: {score}/{count}\n")
@@ -231,7 +232,8 @@ def game(word_size, words_list, readings_list, definitions_list):
             input("Press any key to return to the main menu...\n")
             break
         else:
-            clear()
+            print(answer)
+            clear() #temp
             print(f"{BRIGHT_RED}incorrect!{RESET} The correct reading is: {reading}")
             print(f"{BRIGHT_CYAN}{word}{RESET} ({definition})\n")
             print(f"Score: {score}/{count}\n")
